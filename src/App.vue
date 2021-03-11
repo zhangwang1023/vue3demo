9be0580f0a998e23b9e5b391d88c466336955ad0
zhangwang <zhanglyza@163.com>
2021-03-11 14:40:30 +0800

    组合API-2

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,62 @@
 <template>
   <div>
-    <div>{{count}}</div>
+    <div>{{ count }}</div>
     <button @click="add">点击+1</button>
-    <div v-for="(item,index) in arr.strs" :key="index">
-      {{item}}
+    <div v-for="(item, index) in arr.strs" :key="index">
+      {{ item }}
       <button @click="remove(index)">删除</button>
     </div>
+    <input type="text" v-model="arr2.str">
+    <button @click="addList">添加</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent,ref,reactive } from 'vue';
+import { defineComponent, ref, reactive } from 'vue'
 
 export default defineComponent({
   name: 'App',
-  setup(){
-    let {count,add} = addN()
-    let {arr,remove} = removeN()
-    
-    return{
+  setup() {
+    let { count, add } = addCount()
+    let { arr, remove } = removeN()
+    let { arr2,addList} = addN(arr)
+    return {
       count,
-      arr,
       add,
-      remove
+      arr,
+      remove,
+      arr2,
+      addList
     }
-  }
+  },
 })
-function addN(){
+function addCount() {
   //ref监听简单数据类型 对象或数组用reactive
-    let count = ref<number>(1)
-    const add = ():void=>{
-      count.value++
-    }
-    return {count,add}
+  let count = ref<number>(1)
+  const add = (): void => {
+    count.value++
+  }
+  return { count, add }
 }
-function removeN(){
+function removeN() {
   let arr = reactive({
-      strs:['aaa','bbb','ccc']
-    })
-    const remove = (index:number):void=>{
-      arr.strs.splice(index,1)
-    }
-    return{arr,remove}
+    strs: ['aaa', 'bbb', 'ccc'],
+  })
+  const remove = (index: number): void => {
+    arr.strs.splice(index, 1)
+  }
+  return { arr, remove }
+}
+function addN(arr:any){
+  let arr2 = reactive({
+    str:''
+  })
+  const addList = ():void=>{
+    arr.strs.push(arr2.str)
+    arr2.str = ''
+  }
+  return {arr2,addList}
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
